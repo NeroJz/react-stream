fa34f8c49321860bbd25e48b4a1788198c3a3b41
kumhoe.lau <kumhoe.lau@gdeasia.com>
2020-06-01 21:55:15 +0800

    feat:Wire up Google OAuth
    Create Google OAuth widget.
    Initialize Google OAuth client service.

---
+++ client/src/components/GoogleAuth/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "990499559164-r54p1l5b1f41f4blsco89jv76ouug7nv.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
