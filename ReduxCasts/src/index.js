f4af47ca25c3ad9b09feca9bdb34305087906a3a
kumhoe.lau <nerojz86@gmail.com>
2020-06-22 21:52:39 +0800

    feat: Add YouTube API library and key

--- ReduxCasts/src/index.js
+++ ReduxCasts/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+const API_KEY = "AIzaSyAthC2h_xMKBwoYFb2hmTv-EbunOMBfWXI";
 
 // Create a new component.  This component should produce
 // some HTML
 const App = () => {
   return <div>Hi!</div>;
-}
+};
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector(".container"));
