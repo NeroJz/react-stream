d90d9aaf92289b92a1563411fadae1583fd3b395
kumhoe.lau <nerojz86@gmail.com>
2020-06-25 11:15:12 +0800

    feat:Wired up ActiveBook reducer to Book Detail component

--- react-book/src/reducers/reducer_books.js
+++ react-book/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: "JavaScript: The Good Parts" },
-    { title: "Harry Porter" },
-    { title: "The Dark Tower" },
-    { title: "Eloquent Ruby" },
+    { title: "JavaScript: The Good Parts", pages: 101 },
+    { title: "Harry Porter", pages: 250 },
+    { title: "The Dark Tower", pages: 80 },
+    { title: "Eloquent Ruby", pages: 300 },
   ];
 }
