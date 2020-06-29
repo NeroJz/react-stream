0247000dfc9c1711f63d528f652edf00164d0939
kumhoe.lau <nerojz86@gmail.com>
2020-06-29 14:52:04 +0800

    feat:Install axios dependency

--- react-weather/src/containers/search_bar.js
+++ react-weather/src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { fetchWeather } from "../actions";
 
-export default class SearchBar extends Component {
+export class SearchBar extends Component {
   constructor(props) {
     super(props);
 
@@ -15,6 +18,9 @@ export default class SearchBar extends Component {
 
   onFormSubmit = (event) => {
     event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: "" });
   };
 
   render() {
@@ -35,3 +41,14 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      fetchWeather,
+    },
+    dispatch
+  );
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
