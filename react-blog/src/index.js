df97586b27673bcaae7b5351af9769c9e5aab281
kumhoe.lau <nerojz86@gmail.com>
2020-07-01 20:39:39 +0800

    bugfix: Fixed routing issues

--- react-blog/src/index.js
+++ react-blog/src/index.js
@@ -2,21 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Router, browserHistory } from "react-router";
 import promise from "redux-promise";
 
 import reducers from "./reducers";
 import PostsIndex from "./components/posts_index";
+import PostsNew from "./components/posts_new";
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
+    <Router history={browserHistory}>
       <div>
-        <Route path="/" component={PostsIndex} />
+        <Route path="/" exact={true} component={PostsIndex} />
+        <Route path="/posts/new" component={PostsNew} />
       </div>
-    </BrowserRouter>
+    </Router>
   </Provider>,
   document.querySelector(".container")
 );
