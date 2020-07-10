a4335e312dd749a629184b283006b24e7aa5de83
kumhoe.lau <nerojz86@gmail.com>
2020-07-10 08:59:57 +0800

    refact: Refact Search functional component

--- widget/src/components/Search.js
+++ widget/src/components/Search.js
@@ -3,9 +3,20 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("ReactJS");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
   //   console.log(results);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -15,27 +26,17 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
 
       setResults(data.query.search);
     };
 
-    if (term && !results.length) {
+    if (debouncedTerm) {
       search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 500);
-
-      return () => {
-        clearTimeout(timeoutId);
-      };
     }
-  }, [term]);
+  }, [debouncedTerm]);
 
   const onTextChange = (value) => {
     setTerm(value);
